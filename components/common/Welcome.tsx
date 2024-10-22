10a48b2a1c63d05416a7d9177dd650ab61f9c805
prince thomas <princethomas@autobse.com>
2024-10-22 13:38:54 +0530

    emty commit

--- components/common/Welcome.tsx
+++ components/common/Welcome.tsx
@@ -17,7 +17,7 @@ export default function Welcome() {
   useEffect(() => {
     if (typeof window !== "undefined") {
       const name = localStorage.getItem("name");
-
+ 
       setName(name);
     }
   }, []);
